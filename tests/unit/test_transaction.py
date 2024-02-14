7a1365da2e4eeb13e385b58782641eb9a2115cb4
Javier-Tan <47554099+Javier-Tan@users.noreply.github.com>
2024-02-14 14:39:52 +1100

    Refactor test to use pytext fixture

--- tests/unit/test_transaction.py
+++ tests/unit/test_transaction.py
@@ -1,4 +1,6 @@
 """Provide testing for all classes in transaction module."""
+import pytest
+
 from expy_tui.transaction.transaction import Transaction
 
 
@@ -10,37 +12,14 @@ class TestTransaction:
     date_epoch_time: int = 946684800
     category: str = "Test category"
     description: str = "Test description"
-    value_cents: float = 9999  # In cents
-    cc_value_cents: float = 8888  # In cents
+    value_cents: float = 9999
+    value_dollars: float = 99.99
+    cc_value_cents: float = 8888
+    cc_value_dollars: float = 88.88
 
-    def test_get_value_dollars(self) -> None:
-        test_value_cents: int = 7777
-        test_value_dollar: float = 77.77
-        test_transaction = Transaction(
-            _id=self._id,
-            date=self.date_epoch_time,
-            category=self.category,
-            description=self.description,
-            value=test_value_cents,
-            cc_value=self.cc_value_cents,
-        )
-        assert test_transaction.value == test_value_dollar
-
-    def test_get_cc_value_dollars(self) -> None:
-        test_cc_value_cents: int = 7777
-        test_cc_value_dollar: float = 77.77
-        test_transaction = Transaction(
-            _id=self._id,
-            date=self.date_epoch_time,
-            category=self.category,
-            description=self.description,
-            value=self.value_cents,
-            cc_value=test_cc_value_cents,
-        )
-        assert test_transaction.cc_value == test_cc_value_dollar
-
-    def test_get_value_cents(self) -> None:
-        test_transaction = Transaction(
+    @pytest.fixture()
+    def sample_transaction(self) -> Transaction:
+        return Transaction(
             _id=self._id,
             date=self.date_epoch_time,
             category=self.category,
@@ -48,46 +27,27 @@ class TestTransaction:
             value=self.value_cents,
             cc_value=self.cc_value_cents,
         )
-        test_transaction_value_cents = test_transaction.get_value_cents()
-        assert test_transaction_value_cents == self.value_cents
 
-    def test_get_cc_value_cents(self) -> None:
-        test_transaction = Transaction(
-            _id=self._id,
-            date=self.date_epoch_time,
-            category=self.category,
-            description=self.description,
-            value=self.value_cents,
-            cc_value=self.cc_value_cents,
-        )
-        test_transaction_cc_value_cents = test_transaction.get_cc_value_cents()
-        assert test_transaction_cc_value_cents == self.cc_value_cents
+    def test_get_value_dollars(self, sample_transaction: Transaction) -> None:
+        assert sample_transaction.value == self.value_dollars
 
+    def test_get_cc_value_dollars(self, sample_transaction: Transaction) -> None:
+        assert sample_transaction.cc_value == self.cc_value_dollars
 
-    def test_set_value_cents(self) -> None:
-        test_transaction = Transaction(
-            _id=self._id,
-            date=self.date_epoch_time,
-            category=self.category,
-            description=self.description,
-            value=self.value_cents,
-            cc_value=self.cc_value_cents,
-        )
+    def test_get_value_cents(self, sample_transaction: Transaction) -> None:
+        assert sample_transaction.get_value_cents() == self.value_cents
+
+    def test_get_cc_value_cents(self, sample_transaction: Transaction) -> None:
+        assert sample_transaction.get_cc_value_cents() == self.cc_value_cents
+
+    def test_set_value_cents(self, sample_transaction: Transaction) -> None:
         test_value_cents: int = 7777
         test_value_dollars: float = 77.77
-        test_transaction.set_value_cents(value=test_value_cents)
-        assert test_transaction.value == test_value_dollars
+        sample_transaction.set_value_cents(value=test_value_cents)
+        assert sample_transaction.value == test_value_dollars
 
-    def test_set_cc_value_cents(self) -> None:
-        test_transaction = Transaction(
-            _id=self._id,
-            date=self.date_epoch_time,
-            category=self.category,
-            description=self.description,
-            value=self.value_cents,
-            cc_value=self.cc_value_cents,
-        )
-        test_cc_value_cents: int = 7777
-        test_cc_value_dollars: float = 77.77
-        test_transaction.set_cc_value_cents(value=test_cc_value_cents)
-        assert test_transaction.cc_value == test_cc_value_dollars
+    def test_set_cc_value_cents(self, sample_transaction: Transaction) -> None:
+        test_cc_value_cents: int = 5555
+        test_cc_value_dollars: float = 55.55
+        sample_transaction.set_cc_value_cents(value=test_cc_value_cents)
+        assert sample_transaction.cc_value == test_cc_value_dollars
