faaabe935f333cfff8c8db81b3f73bf6e7c6de72
Javier-Tan <47554099+Javier-Tan@users.noreply.github.com>
2024-02-29 01:55:33 +1100

    Change tests to be implementation agnostic

--- tests/integration/test_transaction_crud.py
+++ tests/integration/test_transaction_crud.py
@@ -2,7 +2,7 @@
 import pytest
 
 from expy_tui.transaction.transaction import Transaction
-from expy_tui.transaction.transaction_crud import TransactionSQLite
+from expy_tui.transaction.transaction_crud import TransactionCRUD, TransactionSQLite
 
 
 class TestTransactionCRUD:
@@ -17,7 +17,7 @@ class TestTransactionCRUD:
     # Tests from here on are dependent on this create test passing.
     # It's recommended to run these tests as a complete file
     @pytest.mark.order(1)
-    def test_create_and_get_all_transactions(self,  inmemory_sqlite_db: TransactionSQLite,
+    def test_create_and_get_all_transactions(self,  inmemory_sqlite_db: TransactionCRUD,
                                              sample_transactions: list[Transaction]) -> None:
 
         # DB should be empty before any creates
@@ -31,13 +31,13 @@ class TestTransactionCRUD:
         no_filter_output = inmemory_sqlite_db.get_transactions_filters()
         assert no_filter_output == sample_transactions
 
-    def test_create_transactions_failure(self,  inmemory_sqlite_db: TransactionSQLite,
+    def test_create_transactions_failure(self,  inmemory_sqlite_db: TransactionCRUD,
                                          sample_transactions: list[Transaction]) -> None:
 
         # Test failure by existing t_id
         assert inmemory_sqlite_db.create_transaction(sample_transactions[0]) is False
 
-    def test_get_transactions_date_filter(self, inmemory_sqlite_db: TransactionSQLite,
+    def test_get_transactions_date_filter(self, inmemory_sqlite_db: TransactionCRUD,
                                           sample_transactions: list[Transaction]) -> None:
         # Test date_range filters
         # Because transactions are ordered by increasing date, we can test by index order
@@ -51,7 +51,7 @@ class TestTransactionCRUD:
         date_filter_output = inmemory_sqlite_db.get_transactions_filters(date_range=date_range)
         assert date_filter_output == expected_transactions_date_range
 
-    def test_get_transactions_categories_filter(self, inmemory_sqlite_db: TransactionSQLite,
+    def test_get_transactions_categories_filter(self, inmemory_sqlite_db: TransactionCRUD,
                                                 sample_transactions: list[Transaction]) -> None:
         # We know "Test category1" and "Test category2" can be found from indexes 8 to end
         start_index = 8
@@ -62,7 +62,7 @@ class TestTransactionCRUD:
         categories_filter_output = inmemory_sqlite_db.get_transactions_filters(categories=categories)
         assert categories_filter_output == expected_transactions_categories
 
-    def test_get_transactions_value_filter(self, inmemory_sqlite_db: TransactionSQLite,
+    def test_get_transactions_value_filter(self, inmemory_sqlite_db: TransactionCRUD,
                                                 sample_transactions: list[Transaction]) -> None:
         # Test value_range filters
         # We know transactions start at 4444 and increase by 1111 every 6 transactions
@@ -74,7 +74,7 @@ class TestTransactionCRUD:
         value_filter_output = inmemory_sqlite_db.get_transactions_filters(value_range=value_range)
         assert value_filter_output == expected_transactions_value_range
 
-    def test_get_transactions_multiple_filters(self, inmemory_sqlite_db: TransactionSQLite,
+    def test_get_transactions_multiple_filters(self, inmemory_sqlite_db: TransactionCRUD,
                                                 sample_transactions: list[Transaction]) -> None:
         start_date = sample_transactions[0].get_date_epoch() # 2000
         end_date = sample_transactions[12].get_date_epoch() # 2012
@@ -129,7 +129,7 @@ class TestTransactionCRUD:
                                                                                           value_range = value_range)
         assert date_categories_value_filter_output == expected_transactions_category_date_value
 
-    def test_get_transaction_id_valid(self, inmemory_sqlite_db: TransactionSQLite,
+    def test_get_transaction_id_valid(self, inmemory_sqlite_db: TransactionCRUD,
                                       sample_transactions: list[Transaction]) -> None:
 
         # Test get_transaction_id method
@@ -139,13 +139,13 @@ class TestTransactionCRUD:
         assert id_one_output == sample_transactions[0]
         assert id_ten_output == sample_transactions[9]
 
-    def test_get_transaction_id_invalid(self, inmemory_sqlite_db: TransactionSQLite) -> None:
+    def test_get_transaction_id_invalid(self, inmemory_sqlite_db: TransactionCRUD) -> None:
 
         id_invalid_output = inmemory_sqlite_db.get_transaction_id(25)
 
         assert id_invalid_output is None
 
-    def test_update_transaction_success(self, inmemory_sqlite_db: TransactionSQLite,
+    def test_update_transaction_success(self, inmemory_sqlite_db: TransactionCRUD,
                                         sample_transactions: list[Transaction]) -> None:
 
         # Test successful update transaction
@@ -160,7 +160,7 @@ class TestTransactionCRUD:
 
         assert inmemory_sqlite_db.update_transaction(id_one_transaction) is True
 
-    def test_update_transaction_failure(self, inmemory_sqlite_db: TransactionSQLite,
+    def test_update_transaction_failure(self, inmemory_sqlite_db: TransactionCRUD,
                                         sample_transactions: list[Transaction]) -> None:
 
         id_one_transaction = sample_transactions[0]
@@ -179,7 +179,7 @@ class TestTransactionCRUD:
         # Revert
         id_one_transaction.t_id = 1
 
-    def test_delete_transaction_success(self, inmemory_sqlite_db: TransactionSQLite,
+    def test_delete_transaction_success(self, inmemory_sqlite_db: TransactionCRUD,
                                         sample_transactions: list[Transaction]) -> None:
 
         # Test successful delete transaction
@@ -190,7 +190,7 @@ class TestTransactionCRUD:
         # Add it back
         inmemory_sqlite_db.create_transaction(id_one_transaction)
 
-    def test_delete_transaction_failure(self, inmemory_sqlite_db: TransactionSQLite,
+    def test_delete_transaction_failure(self, inmemory_sqlite_db: TransactionCRUD,
                                         sample_transactions: list[Transaction]) -> None:
 
         id_one_transaction = sample_transactions[0]
